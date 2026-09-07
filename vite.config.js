7c98085339da9a7d068d78274390c5a26eb208ef
Everaldino Oliveira dos Santos <dino@MacBook-Pro-de-Everaldino.local>
2026-09-07 17:19:24 -0300

    minificando o htmle plugin para isso

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import simpleHtmlPlugin from 'vite-plugin-simple-html';
 
 export default defineConfig({
+  plugins: [
+    simpleHtmlPlugin({
+      minify: true
+    })
+  ],  
   build: {
     outDir: 'docs',
   }
